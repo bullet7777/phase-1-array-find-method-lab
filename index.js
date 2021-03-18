d0d223bd384c793264b878b54d5d81bf27ead8df
chris <chris.mahfouz@hotmail.com>
2021-03-18 14:54:46 +0200

    completed assignment array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+function superbowlWin(arrayOfObjects){
+
+    function isWinningObject(element){
+        var k = element.result === "W"; 
+        return k;
+    }
+
+    var result = arrayOfObjects.find(isWinningObject)
+if(result===undefined){
+    return undefined
+}
+    return result.year
+
+}
+
